3e1dacb21e9d0e4cca1e29c07280f61ef912eae2
Facundo Zuleta <facundozuleta70@gmail.com>
2025-11-03 17:02:22 -0300

    cors config

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,15 +1,18 @@
 import { CorsOptions } from 'cors'
 
+const ACCEPTED_ORIGINS = [
+    process.env.FRONTEND_URL || 'http://localhost:5173'
+]
+
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        const whitheList = [process.env.FRONTEND_URL]
-        if (process.argv[2] === '--api') {
-            whitheList.push(undefined)
-        }
-        if (whitheList.includes(origin)) {
+        const whiteList = ACCEPTED_ORIGINS
+
+        if (whiteList.includes(origin) || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Error de CORS'))
         }
-    }
+    },
+    credentials: true
 }
